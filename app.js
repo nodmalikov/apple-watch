21dd32909c98adaf697d5cd5db5cb5706bcd15c6
nodmalikov <abcmalikov@gmail.com>
2024-08-23 14:19:31 +0500

    Add site head icon and loader

--- app.js
+++ app.js
@@ -50,3 +50,18 @@ if (elSitenav) {
     document.addEventListener('touchstart', handleTouchStart, false);
     document.addEventListener('touchmove', handleTouchMove, false);
 }
+
+// LOADER
+document.onreadystatechange = function () {
+    if (document.readyState !== "complete") {
+        document.querySelector(
+            "body").style.visibility = "hidden";
+        document.querySelector(
+            "#loader").style.visibility = "visible";
+    } else {
+        document.querySelector(
+            "#loader").style.display = "none";
+        document.querySelector(
+            "body").style.visibility = "visible";
+    }
+};
